f93f60746198c8e6f3dceafd697a56762ab0e03d
unknown <gneolxiong@gmail.com>
2023-01-30 03:34:15 -0600

    Made changes

--- functions.js
+++ functions.js
@@ -1,11 +1,12 @@
-function myFunction() {
+function myFunction() { //This function is to return last time it was modified
     document.getElementById("mod_time").innerHTML = document.lastModified;
 }
 
-function darkMode() {
+var theme; //this variable is global so that textChange() can access it
+function darkMode() { //This function is to enable toggle on/off for dark mode
   var element = document.body;
   element.classList.toggle("dark-mode");
-  var theme;
+  
 
   if(element.classList.contains("dark-mode")){
     console.log("Dark Mode");
@@ -24,5 +25,13 @@ if(GetTheme === "DARK"){
   document.body.classList = "dark-mode";
 }
 
+function textChange(){ //This function changes the text on the button to show whether dark mode is on/off
+  var y = document.getElementById("buttonDIV");
+  if(theme == "DARK"){
+    y.innerHTML = "Dark Mode On";
+  }else{
+    y.innerHTML = "Dark Mode Off";
+  }
+}
+
 myFunction()
-//darkMode()
